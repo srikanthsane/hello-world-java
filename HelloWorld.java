afe100f3c5222a516d41f12ca9aaa0b28e8117e7
srikanthsane <srikanthsane98@gmail.com>
2026-02-12 16:06:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("srikanth made the chnages in the git hub code Hello world!");
   }
 }
