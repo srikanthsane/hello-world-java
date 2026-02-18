9cd64345ab79dc347abfbfb065957e34914fd3a1
srikanthsane <srikanthsane98@gmail.com>
2026-02-18 14:00:28 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("srikanth made the chnages in the git hub code with the webhook Hello world!");
+    System.out.println("srikanth made the chnages in the git Hello world!");
   }
 }
